e5d660049576483a26c90f30be88b2da30ad791b
clf94-dev <clf94@hotmail.es>
2020-10-22 10:09:08 +0200

    added navbar struct

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  logo='../../../assets/images/shared/desktop/logo.svg';
+  constructor(private _route: ActivatedRoute, private _router: Router){ }
 
   ngOnInit(): void {
   }
-
+  goToHome(){
+    this._router.navigate(['/home']);
+  }
 }
