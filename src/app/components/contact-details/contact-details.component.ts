70bd5eaf25324708f05a8ec83173ea9e336ec7d3
clf94-dev <clf94@hotmail.es>
2020-10-26 07:47:30 +0100

    added contactInfo data

--- src/app/components/contact-details/contact-details.component.ts
+++ src/app/components/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactInfo } from './contactInfo';
 
 @Component({
   selector: 'app-contact-details',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-details.component.scss']
 })
 export class ContactDetailsComponent implements OnInit {
-
-  constructor() { }
+public contactinfo: ContactInfo[];
+  constructor() {
+this.contactinfo=[
+  new ContactInfo ("Main Office","Mail :			archone@mail.com","Address :		1892  Chenoweth Drive TN","Phone :		123-456-3451"),
+  new ContactInfo ("Office II","Mail :			archtwo@mail.com","Address :		3399  Wines Lane TX","Phone :		832-123-4321")
+]
+   }
 
   ngOnInit(): void {
   }
---
+++ src/app/components/contact-details/contactInfo.ts
@@ -0,0 +1,10 @@
+export class ContactInfo{
+constructor(
+
+    public office:string,
+    public mail:string,
+    public address:string,
+    public phone:string,
+
+){}
+}
