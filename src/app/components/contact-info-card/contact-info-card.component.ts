5e8eab9ce457c2b83d4d34ffecc7cfcbca75a913
clf94-dev <clf94@hotmail.es>
2020-10-26 07:42:21 +0100

    added contactInfoCard struct component

--- src/app/components/contact-info-card/contact-info-card.component.ts
+++ src/app/components/contact-info-card/contact-info-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-info-card',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-info-card.component.scss']
 })
 export class ContactInfoCardComponent implements OnInit {
-
+@Input() office: string;
+@Input() mail: string;
+@Input() address: string;
+@Input() phone: string;
   constructor() { }
 
   ngOnInit(): void {
