7dd3ee38190e45117a0cc56f625c108483a9f5b1
clf94-dev <clf94@hotmail.es>
2020-10-27 08:58:33 +0100

    added struct introHome component

--- src/app/components/home-intro/home-intro.component.ts
+++ src/app/components/home-intro/home-intro.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-intro',
   templateUrl: './home-intro.component.html',
-  styleUrls: ['./home-intro.component.scss']
+  styleUrls: ['./home-intro.component.scss'],
 })
 export class HomeIntroComponent implements OnInit {
+  constructor(private _route: ActivatedRoute, private _router: Router) {}
+  public page: number;
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  goToPortfolio() {
+    this._router.navigate(['/portfolio']);
   }
-
+  goToPage1() {
+    this.page = 0;
+  }
+  goToPage2() {
+    this.page = 1;
+  }
+  goToPage3() {
+    this.page = 2;
+  }
+  goToPage4() {
+    this.page = 3;
+  }
 }
