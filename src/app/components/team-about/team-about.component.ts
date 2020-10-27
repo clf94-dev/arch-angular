9012d54c1fdc541a214f82f9fd498936c6e9f762
clf94-dev <clf94@hotmail.es>
2020-10-27 08:12:09 +0100

    added members data

--- src/app/components/team-about/team-about.component.ts
+++ src/app/components/team-about/team-about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamData } from './teamData';
 
 @Component({
   selector: 'app-team-about',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-about.component.scss']
 })
 export class TeamAboutComponent implements OnInit {
-
-  constructor() { }
+public membersdata: TeamData[];
+  constructor() {
+    this.membersdata=[
+      new TeamData("Jake Richards","Chief Architect","../../../assets/images/about/desktop/avatar-jake.jpg","Jake Richards"),
+      new TeamData("Thompson Smith","Head of Finance","../../../assets/images/about/desktop/avatar-thompson.jpg","Thompson Smith"),
+      new TeamData("Jackson Rourke","Lead Designer","../../../assets/images/about/desktop/avatar-jackson.jpg","Jackson Rourke"),
+      new TeamData("Maria Simpson","Senior Architect","../../../assets/images/about/desktop/avatar-maria.jpg","Maria Simpson")
+    ]
+   }
 
   ngOnInit(): void {
   }
---
+++ src/app/components/team-about/teamData.ts
@@ -0,0 +1,8 @@
+export class TeamData{
+    constructor(
+        public name: string,
+        public position: string,
+        public src: string,
+       public imgAlt:string
+    ){}
+}
